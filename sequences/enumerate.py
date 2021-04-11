e124214440e368776f0a9bc628ac430dafd2178f
sergiosanchezbarradas <sergio_sanchez_b@hotmail.com>
2021-04-12 09:28:51 +1000

    12apr hplaptop

--- sequences/enumerate.py
+++ sequences/enumerate.py
@@ -1,2 +1,5 @@
-for index, character in enumerate("abcdefghijklmnopqrstuvwxyz"):
-    print(index + 1, character)
+# for index, character in enumerate("abcdefghijklmnopqrstuvwxyz"):
+#     print(index + 1, character)
+
+for i in enumerate("abcdefghijklmnopqrstuvwxyz"):
+    print(i)
---
+++ sequences/unpacking.py
@@ -0,0 +1,20 @@
+x, y, z = 12,45,88
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a tuple:")
+
+data = 22,556,234,
+x, y, z = data
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a list:")
+data_list = [367,86,3]
+p, q, r = data_list
+print(p)
+print(q)
+print(r)
+
